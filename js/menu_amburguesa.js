033feeceed2a9afce32b5eb9f05f8b6fcf894222
DanielPaezb <dcpaez595@misena.edu.co>
2020-12-08 16:28:53 -0500

    * Se agrega menú amburguesa al portafolio.  * Proyecto responsive

---
+++ js/menu_amburguesa.js
@@ -0,0 +1,4 @@
+// Menú hamburguesa //
+const iconNav = document.getElementById("iconNav");
+const navbar = document.getElementById("navbar");
+iconNav.addEventListener("click", () => navbar.classList.toggle("ul--dBlock"));
