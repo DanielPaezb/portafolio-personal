3606007ac499512c7007655de65533b8ea50a994
DanielPaezb <dcpaez595@misena.edu.co>
2021-01-15 09:48:26 -0500

    * Se cambia completamente la interfáz gráfica *  *    del portafolio, todavía en desarrollo    *

--- js/menu_amburguesa.js
+++
@@ -1,4 +0,0 @@
-// Menú hamburguesa //
-const iconNav = document.getElementById("iconNav");
-const navbar = document.getElementById("navbar");
-iconNav.addEventListener("click", () => navbar.classList.toggle("ul--dBlock"));
